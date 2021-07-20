329a251e48dfd15ee07494f4fcdad3d688bdc271
wankcn <wankcn@icloud.com>
2021-07-20 23:42:41 +0800

    时间

--- Lua/Test.lua
+++ Lua/Test.lua
@@ -1,16 +1,3 @@
-print("这是TestLua脚本")
+local a = os.date("%Y/%m/%d %X")
 
--- 全局变量
-testA = "123"
-
--- 局部变量
-local testLocalA = "456"
-print(testLocalA)
-
-
-
-
-aa = {1,2,3,4,5,6s
-
--- 把本地变量返回出去
-return testLocalA
+print(a)
