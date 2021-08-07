a5824ada7050e0770d4d3cb14886a1fd4c26383f
wankcn <wankcn@icloud.com>
2021-08-08 01:12:32 +0800

    [lua] 多态补充

--- Lua/Lesson15_OO.lua
+++ Lua/Lesson15_OO.lua
@@ -38,6 +38,7 @@ function Object:SubClass(className)
 	_G[className] = {}
 	local obj = _G[className]
 	self.__index = self
+	obj.base = self                   -- [用于多态] 定义一个base属性等于表
 	setmetatable(obj,self)            -- 谁调用，元表就是谁
 end
 
@@ -49,19 +50,34 @@ local per = Person:New()              -- Person中没有new方法，其实调用
 print("per.id:",per.id)               -- 从Person中取值，娶不到继续Person的元表Object中取
 
 -- /////////////////////////////////// [ 多态 ] ///////////////////////////////////
-Object:SubClass("GO")
+
+Object:SubClass("GO")                 -- 继承object
 GO.posX = 0
 GO.posY = 1
 
 function GO:Move()
-	self.posY = self.
+	self.posY = self.posY + 1
+	self.posX = self.posX + 1
+	print("posX: ".. self.posX,"posY: " .. self.posY)
 end
 
+GO:SubClass("Player")                 -- 继承GO
 
+function Player:Move()
+	print("Player 重写的方法！")
+--	self.base:Move()                  -- 冒号默认传第一个参数是self，相当于把基类传进去 相当于GO
 
+	self.base.Move(self)              -- 用.调用传入self,不传self相当于通过元表修改的父类中的值
+	                                     -- self 是调用者本身 保留父类方法需要用.来调用
 
+end
 
+local p1 = Player:New()
+p1:Move()                             -- 这里打印 1，2
 
+-- 不同对象使用的成员变量是相同的 
+local p2 = Player:New()
+p2:Move()                             -- 这里打印 2，3
 
 
 
