7f923d0e5fca71d6abae8e58c603537ff7ec6232
wankcn <wankcn@icloud.com>
2021-08-10 00:58:08 +0800

    [lua] 数学运算

--- Lua/lesson16_Library.lua
+++ Lua/lesson16_Library.lua
@@ -1,5 +1,7 @@
 --- 自带库
 
+-- /////////////////////////////////// [ 时间 ] ///////////////////////////////////
+
 local time = os.time()
 print("当前时间：",time)
 
@@ -14,6 +16,44 @@ for k ,v in pairs(nowTime) do
 	print(k,v)
 end
 
-
-
 print(os.date("%Y-%m-%d, %H:%M:%S",os.time()))
+
+-- /////////////////////////////////// [ 数学运算 ] ///////////////////////////////////
+-- math公共类
+-- 绝对值
+print("abs绝对值：",math.abs(-11))
+
+-- 弧度转角度
+print("弧度转角度：",math.deg(math.pi))
+
+-- 三角函数 传弧度
+print(math.cos(math.pi))
+
+-- 向上取整，向下取整
+print(math.floor(2.6))
+print(math.ceil(2.6))
+
+-- 最大值最小值
+print(math.max(1,2))
+print(math.min(4,5))
+
+-- 把小数分成整数和小数
+print(math.modf(3.4))
+-- 幂运算
+print(math.pow(2,5))
+
+-- 随机数
+-- 先设置随机数种子
+math.randomseed(os.time())
+print(math.random(100))
+
+-- 平方根
+print(math.sqrt(4))
+
+-- /////////////////////////////////// [ 数学运算 ] ///////////////////////////////////
+
+
+
+
+
+
