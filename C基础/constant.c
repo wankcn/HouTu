4f36c7ad2e79f71085f5a480a8cff14061599ed5
wankcn <wankcn@icloud.com>
2021-11-09 15:59:24 +0800

    add注释

--- C基础/constant.c
+++ C基础/constant.c
@@ -4,15 +4,16 @@
 
 #include<stdio.h>
 
-#define RED 0xFF0000
+#define RED 0xFF0000 // 宏修饰
 
 int main() {
+    // volatile只读值
     volatile const int red = 0xFF0000;
     printf("source %d\n", red);
 
     int *p = &red;
     *p = 0;
-    printf("exchange %d\n", red);
+    printf("exchange %d\n", red);  // macos保护常量
     printf("define red %d\n", RED);
 
     return 0;
